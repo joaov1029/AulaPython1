c40230e6bc64a85fa434a24a6e641d2b2aff2b46
joaov-1029 <joaovictor.sallesherculano@gmail.com>
2023-10-09 22:01:53 -0300

    barraca de frutas

--- Exercicio_fruta.py
+++ Exercicio_fruta.py
@@ -1,23 +1,6 @@
-"""
-Um programa que vende frutas:
-nome do comprador
-idade do comprador
-perguntar a fruta desejada
-menu
-1.) banana
-2.) maçã
-3.) pêra
-4.) melância
-5.) morango
-6.) Abacaxi
-
-Escolher fruta
-apresentar o nome da pessoa, a idade e a  fruta escolhida
-"""
-
 nome = input("Digite seu nome:\n")
 idade = int(input("Digite sua idade\n"))
-fruta = input("Qual das frutas abaixo deseja comprar?"
+fruta = input("\nQual das frutas abaixo deseja comprar?"
               "\n1.) banana"
               "\n2.) maçã"
               "\n3.) pêra"
