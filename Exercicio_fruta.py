343a62e012a4100b7626e914705c4eaf2719df8d
joaov-1029 <joaovictor.sallesherulano@gmail.com>
2023-10-16 21:03:54 -0300

    Uso de lista

--- Exercicio_fruta.py
+++ Exercicio_fruta.py
@@ -1,3 +1,12 @@
+frutas = {
+    "1": "banana",
+    "2": "maçã",
+    "3": "pêra",
+    "4": "melância",
+    "5": "morango",
+    "6": "Abacaxi"
+}
+
 nome = input("Digite seu nome:\n")
 idade = int(input("Digite sua idade\n"))
 fruta = input("\nQual das frutas abaixo deseja comprar?"
@@ -8,17 +17,7 @@ fruta = input("\nQual das frutas abaixo deseja comprar?"
               "\n5.) morango"
               "\n6.) Abacaxi"
               "\n")
-if fruta == "1" or fruta == "banana":
-    print(f"\nO cliente {nome}, com a idade {idade} anos, deseja comprar banana")
-elif fruta == "2" or fruta == "maçã":
-    print(f"\nO cliente {nome}, com a idade {idade} anos, deseja comprar maçã")
-elif fruta == "3" or fruta == "pêra":
-    print(f"\nO cliente {nome}, com a idade {idade} anos, deseja comprar pêra")
-elif fruta == "4" or fruta == "melância":
-    print(f"\nO cliente {nome}, com a idade {idade} anos, deseja comprar melância")
-elif fruta == "5" or fruta == "morango":
-    print(f"\nO cliente {nome}, com a idade {idade} anos, deseja comprar morango")
-elif fruta == "6" or fruta == "Abacaxi":
-    print(f"\nO cliente {nome}, com a idade {idade} anos, deseja comprar Abacaxi")
+if fruta in frutas.keys():
+    print(f"\nO cliente {nome}, com a idade {idade} anos, deseja comprar {frutas[fruta]}")
 else:
-    print("Fruta não disponível")
+    print("Fruta não encontrada")
